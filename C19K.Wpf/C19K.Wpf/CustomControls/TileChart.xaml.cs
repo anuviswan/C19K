6d7f9eaf68f573f3d5155c6a89a2f42f805c5fa3
Anu Viswan <anu.viswan@gmail.com>
2020-04-13 18:49:47 +0530

    Working on title control

---
+++ C19K.Wpf/C19K.Wpf/CustomControls/TileChart.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace C19K.Wpf.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for TileChart.xaml
+    /// </summary>
+    public partial class TileChart : UserControl
+    {
+        public TileChart()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public double Value
+        {
+            get { return (double)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Value.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(double), typeof(TileChart), new PropertyMetadata(0, OnPropertyChanged));
+
+
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Title.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(TileChart), new PropertyMetadata(string.Empty, OnPropertyChanged));
+
+
+        public Color TileColor
+        {
+            get { return (Color)GetValue(TileColorProperty); }
+            set { SetValue(TileColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TileColor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TileColorProperty =
+            DependencyProperty.Register("TileColor", typeof(Color), typeof(TileChart), new PropertyMetadata(Colors.Red, OnPropertyChanged));
+
+
+        public Color FontColor
+        {
+            get { return (Color)GetValue(FontColorProperty); }
+            set { SetValue(FontColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for FontColor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FontColorProperty =
+            DependencyProperty.Register("FontColor", typeof(Color), typeof(TileChart), new PropertyMetadata(Colors.White, OnPropertyChanged));
+
+
+        private static void OnPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var instance = d as TileChart;
+            instance.UpdateLayout();
+        }
+
+
+
+    }
+}
